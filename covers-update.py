4fdb80f922c1f979d0489670b42370c07590af90
Adriano Teodoro <adrianopteodoro@gmail.com>
2023-03-12 18:49:16 -0300

    added check for hires (need to enhance)

--- covers-update.py
+++ covers-update.py
@@ -1,6 +1,8 @@
 import re
 import os
 import hashlib
+import logging
+import grequests
 
 import scrapy
 from scrapy import Field, Item, Request
@@ -8,6 +10,8 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.pipelines.images import ImagesPipeline
 from io import BytesIO
 from scrapy.utils.misc import md5sum
+
+logger = logging.getLogger("covers-update")
 
 COVERS_PATH = f"{os.path.abspath(os.path.dirname(__file__))}/covers"
 
@@ -34,49 +38,83 @@ class CoverImageItem(Item):
 class PsxDataCenterCoverSpider(scrapy.Spider):
     name = "psxdatacentercover"
     start_urls = [
-        "https://psxdatacenter.com/images/",
+        "https://psxdatacenter.com/images/covers/",
     ]
 
+    def check_hires(self, hires_front_cover_urls: list):
+        rs = (grequests.get(url) for url in hires_front_cover_urls)
+        return [(res if res.status_code == 200 else None) for res in grequests.map(rs)]
+
+    def parse_ps1serial(
+        self,
+        region: str,
+        letter: str,
+        serial: str,
+        response,
+        href_data: str,
+    ):
+        if not region or not letter or not serial:
+            return
+        hires_results = self.check_hires(
+            [
+                f"https://psxdatacenter.com/images/hires/{region}/{letter}/{serial}/{serial}-F-ALL.jpg",
+                f"https://psxdatacenter.com/images/covers/{region}/{letter}/{serial}/{serial}-F-ALL.jpg",
+            ]
+        )
+
+        hires_request = next(
+            (res for res in hires_results if res is not None), response
+        )
+        image_url = (
+            hires_request.url
+            if "-F-ALL.jpg" in hires_request.url
+            else response.urljoin(href_data)
+        )
+        image_body = (
+            hires_request.content
+            if hasattr(hires_request, "content")
+            else hires_request.body
+        )
+        checksum = md5sum(BytesIO(image_body))
+        local_cover_file = f"{COVERS_PATH}/{serial}.jpg"
+        need_to_download = False
+        if os.path.exists(local_cover_file):
+            local_checksum = ""
+            with open(local_cover_file, "rb") as f:
+                local_checksum = hashlib.file_digest(f, "md5").hexdigest()
+            if checksum != local_checksum:
+                need_to_download = True
+        else:
+            need_to_download = True
+        if need_to_download:
+            return CoverImageItem(
+                serial=serial,
+                image_urls=[image_url],
+                checksum=checksum,
+            )
+
     def parse(self, response):
-        if (
-            response.url == self.start_urls[-1]
-            or f"{self.start_urls[-1]}covers/" in response.url
-            or f"{self.start_urls[-1]}hires/" in response.url
-        ):
+        if f"{self.start_urls[-1]}" in response.url:
             for href in response.xpath("/html/body/pre/a/@href"):
                 href_data = href.get()
                 # follow the directory links
-                if href_data[-1] == "/":
+                if href_data[-1] == "/" and (
+                    not response.urljoin(href_data) == response.url
+                    or response.urljoin(href_data) == self.start_urls[-1]
+                ):
                     yield response.follow(href_data, self.parse)
                 # get only the FRONT cover images
-                ps1serial = None
+                region = ""
+                letter = ""
+                serial = ""
                 try:
-                    format1, format2 = re.findall(
-                        r"([A-Z]{3,4}-[0-9]{1,5})-F-ALL\.jpg|([A-Z]{3,4}-[0-9]{1,5})\.jpg",
-                        href_data,
+                    region, letter, serial = re.findall(
+                        r"([A-Z]{1})\/([A-Z0-9\-]{1,3})\/([A-Z]{3,4}-[0-9]{1,5})\.jpg",
+                        response.urljoin(href_data),
                     )[0]
-                    ps1serial = format1 if format1 else format2
                 except:
                     yield
-                if ps1serial:
-                    checksum = md5sum(BytesIO(response.body))
-                    local_cover_file = f"{COVERS_PATH}/{ps1serial}.jpg"
-                    need_to_download = False
-                    if os.path.exists(local_cover_file):
-                        local_checksum = ""
-                        with open(local_cover_file, "rb") as f:
-                            local_checksum = hashlib.file_digest(f, "md5").hexdigest()
-                        if checksum != local_checksum:
-                            need_to_download = True
-                    else:
-                        need_to_download = True
-                    if need_to_download:
-                        yield CoverImageItem(
-                            serial=ps1serial,
-                            image_urls=[response.urljoin(href_data)],
-                            checksum=checksum,
-                        )
-                yield
+                yield self.parse_ps1serial(region, letter, serial, response, href_data)
 
 
 process = CrawlerProcess(
